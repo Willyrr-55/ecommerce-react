cb0714f6802ad0eb8e8614f64f0f5a222fe29d1e
Willian Adonay Rosa Ramirez <willyrosaramirezc1@gmail.com>
2022-08-10 19:26:46 -0600

    Empezando prueba de react router dom

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+
+const Home = () => <h1>Home</h1>
+
+const SearchPage= ()=> <h1>Search Page</h1>
+
+function App() {
+
+  return (
+    <div className="App"> 
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/search-page" element={<SearchPage />}/>
+    </Routes>
+    
+      
+    </div>
+  )
+}
+
+export default App
